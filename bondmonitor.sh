50b4ec28287da3df5dd674c40e6656716278e90c
Andy Son <andy.son@andyson.org>
2015-08-03 13:03:01 -0500

    Updated bondmonitor.sh to use /proc/net/bonding tree to remove requirement of running from privileged user

--- bondmonitor.sh
+++ bondmonitor.sh
@@ -9,7 +9,19 @@
 #****************************************************************************
 #
 # File:        bondmonitor.sh
-# Description: Checks /var/log/messages for bonding failover events
+# Description: Checks /proc/net/bonding for changes to the primary bond
+#              Initial revision relied on /var/log/messages but the need to
+#              schedule this from a non-privileged account necessitated an
+#              alternative solution. This monitor is specifically only for
+#              active-backup setups
+#
+#              The utility of this script relies on it being scheduled on
+#              a fairly frequent basis as it will only alert on changes from
+#              the previous check
+#
+#              This script relies on being able to keep a record of its last
+#              state in a persistent file defined in ${JOURNAL} variable
+#
 #
 #****************************************************************************
 set -u
@@ -18,55 +30,135 @@ set -u
 # Customizable variable settings
 #----------------------------------------------------------------------------
 
-# messages log file
-MESSAGES=/var/log/messages
+# bonding proc directory
+BONDPROCPATH="/proc/net/bonding"
 
 # journal to track position in the messages file
-JOURNAL=/var/run/bondmonitor.journal
+JOURNALPATH="/var/run/bondmonitor"
+JOURNAL="${JOURNALPATH}/bondmonitor.journal"
 
 #----------------------------------------------------------------------------
 # OS specific definitions
 #----------------------------------------------------------------------------
 
-AWK=/usr/bin/awk
-EXPR=/usr/bin/expr
-GREP=/usr/bin/grep
-TAIL=/usr/bin/tail
-WC=/usr/bin/wc
+AWK="/bin/awk"
+GREP="/bin/grep"
+LS="/bin/ls"
+TAIL="/usr/bin/tail"
+
+CMDLIST="${AWK} ${GREP} ${LS} ${TAIL}"
+
+
+#****************************************************************************
+#       !!!!!!!!!!!!!!END OF CUSTOMIZABLE VARIABLES!!!!!!!!!!!!!!
+#       !!!!!!!!!!DO NOT ALTER ANYTHING BELOW THIS LINE!!!!!!!!!!
+#****************************************************************************
 
 #----------------------------------------------------------------------------
 # Global Variables
 #----------------------------------------------------------------------------
 
-FAILOVERSTRING="the new active one"
+BONDMODESTRING="Bonding Mode: fault-tolerance (active-backup)"
+BONDACTIVESTRING="Currently Active Slave:"
+PREVBONDSTATE=""
+CURRBONDSTATE=""
 
-# Main 
+RETVAL=""
 
-LASTLINE=0
+# Exit states
+EXITERROR=100
+EXITFODETECTED=1
+EXITSUCCESS=0
 
-# Extract the last read position
-if [ -e ${JOURNAL} ] 
+
+#----------------------------------------------------------------------------
+# Main
+#----------------------------------------------------------------------------
+
+# Check configurable OS paths
+OSVARERROR="FALSE"
+
+# verify that all external executables are available
+for CMD in ${CMDLIST}
+do
+  if [[ ! -x ${CMD} ]]
+  then
+    echo 1>&2 "Error: Can not locate OS command: ${CMD}"
+    OSVARERROR="TRUE"
+  fi
+done
+
+if [[ "${OSVARERROR}" = "TRUE" ]]
 then
-  LASTLINE=`${TAIL} -1 ${JOURNAL}`
+  echo 1>&2 "FATAL: Required external OS commands can not be located!"
+  exit ${EXITERROR}  # exit with error
+fi
+
+# Read in previous state
+if [[ -f ${JOURNAL} ]]
+then
+  if [[ -r ${JOURNAL} ]]
+  then
+    PREVBONDSTATE=`${TAIL} -1 ${JOURNAL}`
+  else
+    echo 1>&2 "FATAL: ${JOURNAL} exists but is unreadable. This state file is integral to the proper function of the script. Please correct and re-run."
+    exit ${EXITERROR}
+  fi
 else
-  echo "0" > ${JOURNAL}
+  echo "Initial state" > ${JOURNAL}
+  if [[ $? -ne 0 ]]
+  then
+    echo 1>&2 "FATAL: ${JOURNAL} unwritable. This state file is integral to the proper function of the script. Please correct and re-run."
+    exit ${EXITERROR}
+  fi
 fi
 
-# check filesize in case of log rotation
-CURRENTSIZE=`${WC} -l ${MESSAGES} | ${AWK} '{print($1)}'`
-if [ ${CURRENTSIZE} -lt ${LASTLINE} ]
+
+if [[ ! -d ${BONDPROCPATH} ]]
 then
-  LASTLINE=0
+  echo 1>&2 "Error: Unable to find ${BONDPROCPATH}. Please ensure bonding is enabled in this enviornment."
+  exit ${EXITERROR}
 fi
 
-NUMLINES=`${EXPR} ${CURRENTSIZE} - ${LASTLINE}`
+RETURNSTATUS=${EXITSUCCESS}
+for BONDDEV in `${LS} ${BONDPROCPATH}`
+do
+  BONDACTIVEINT=""
+  if [[ ! -z "`${GREP} \"${BONDMODESTRING}\" ${BONDPROCPATH}/${BONDDEV}`" ]]
+  then
+    BONDACTIVEINT="`${GREP} \"${BONDACTIVESTRING}\" ${BONDPROCPATH}/${BONDDEV} | ${AWK} '{print($4)}'`"
+    CURRBONDSTATE="${BONDDEV}:${BONDACTIVEINT} ${CURRBONDSTATE}"
 
-FAILOVERFOUND=`${TAIL} -n ${NUMLINES} ${MESSAGES} | ${GREP} bonding | ${GREP} "${FAILOVERSTRING}"`
-if [ ! -z "${FAILOVERFOUND}" ]
-then
-  echo "Failover detected: ${FAILOVERFOUND}"
-else
-  echo "No failovers detected"
-fi
+    if [[ "${PREVBONDSTATE}" =~ "${BONDDEV}" ]]
+    then
 
-echo "${CURRENTSIZE}" > ${JOURNAL}
+      FAILOVERDETECTED="FALSE"
+
+      for STATE in ${PREVBONDSTATE}
+      do
+        STATEDEV=`echo ${STATE} | ${AWK} -F: '{print($1)}'`
+        if [[ "${STATEDEV}" = "${BONDDEV}" ]]
+        then
+          STATEINT=`echo ${STATE} | ${AWK} -F: '{print($2)}'`
+          if [[ "${STATEINT}" != "${BONDACTIVEINT}" ]]
+          then
+            FAILOVERDETECTED="TRUE"
+            echo 1>&2 "Failover detected on ${BONDDEV}: Current active interface: ${BONDACTIVEINT} Previous active interface: ${STATEINT}"
+          fi
+        fi
+      done
+
+      if [[ "${FAILOVERDETECTED}" = "TRUE" ]]
+      then
+        RETURNSTATUS=${EXITFODETECTED}
+      fi
+    fi
+  fi
+done
+
+# Make note of current state
+echo "${CURRBONDSTATE}" > ${JOURNAL}
+
+
+exit ${RETURNSTATUS}
+
